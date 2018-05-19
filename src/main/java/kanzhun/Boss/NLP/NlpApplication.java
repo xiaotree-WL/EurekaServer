b4c16f915b171be2309254d309a316c028bfc92b
wanglin01@kanzhun.com <wanglin01@kanzhun.com>
2018-05-19 18:12:30 +0800

    add eureka server

---
+++ src/main/java/kanzhun/Boss/NLP/NlpApplication.java
@@ -0,0 +1,15 @@
+package kanzhun.Boss.NLP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpApplication.class, args);
+	}
+}
